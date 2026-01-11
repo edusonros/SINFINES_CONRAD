69887cbcda2b81a176000d62cf30c9d4d7a5bb17
edusonros <edusonros@gmail.com>
2026-01-11 06:17:20 +0100

    cuasi definitivo

--- exporter/run_inventor.py
+++ exporter/run_inventor.py
@@ -1,25 +1,27 @@
 from pathlib import Path
 
+try:
+    import win32com.client as win32com
+except ModuleNotFoundError:
+    win32com = None
+
 ILOGIC_RULE = "01_Tornillo_Sinfin"
 ASSEMBLY_PATH = Path(
-    r"C:\edusonros_projects\SINFINES_CONRAD\iLogic\Tornillo Sinfin_v001.iam")
+    r"C:\edusonros_projects\SINFINES_CONRAD\iLogic\Tornillo Sinfin_v001.iam"
+)
 
 
 def run_inventor():
     if win32com is None:
         raise RuntimeError("Falta pywin32. Instala con: pip install pywin32")
 
-    import win32com.client
-
-    inv = win32com.client.Dispatch("Inventor.Application")
-    inv.Visible = True  # TRUE como pediste
+    inv = win32com.Dispatch("Inventor.Application")
+    inv.Visible = True
 
     doc = inv.Documents.Open(str(ASSEMBLY_PATH), True)
+    inv.ActiveDocument = doc
 
-    # Ejecutar regla iLogic
-    # Si tu regla está en el IAM, esta llamada funciona:
-    inv.RunRule(ILOGIC_RULE)
+    inv.RunRule("01_Tornillo_Sinfin")
 
-    # Opcional: guardar
     doc.Update()
     doc.Save2(True)
